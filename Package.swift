20c5042439c4fd6e1605b18488625644a5b0e580
Muhammad Azri Fatihah Susanto <Muhammad.azri.f.s@gmail.com>
2025-07-31 12:05:52 +0700

    fix: configure custom source paths in Package.swift
    Add path parameters to target definitions to resolve SPM source location errors

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PokedexPocketPokemon",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "PokedexPocketPokemon",
+            targets: ["PokedexPocketPokemon"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.9.0"),
+        .package(url: "https://github.com/muhAzri/PokedexPocketCore", branch: "main")
+    ],
+    targets: [
+        .target(
+            name: "PokedexPocketPokemon",
+            dependencies: [
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "RxRelay", package: "RxSwift"),
+                .product(name: "PokedexPocketCore", package: "PokedexPocketCore")
+            ],
+            path: "PokedexPocketPokemon"
+        ),
+        .testTarget(
+            name: "PokedexPocketPokemonTests",
+            dependencies: [
+                "PokedexPocketPokemon",
+                .product(name: "RxBlocking", package: "RxSwift"),
+                .product(name: "RxTest", package: "RxSwift")
+            ],
+            path: "PokedexPocketPokemonTests"
+        ),
+    ]
+)
